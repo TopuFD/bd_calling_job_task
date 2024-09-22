3ceff79c5383c6b17df6bcf01be7893af87b5a07
TopuFD <topuroy3317@gmail.com>
2024-09-22 21:04:40 +0600

    complete app

--- lib/controller/google_map_controller.dart
+++ lib/controller/google_map_controller.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:ddddd/controller/device_info_controller.dart';
 import 'package:ddddd/controller/user_current_location.dart';
-import 'package:ddddd/main.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
